eda521dd5646826a6eb2b8fac23b9c6f947df216
ohmycloud <ohmycloudy@gmail.com>
2025-01-14 23:35:33 +0800

    make address configurable

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,7 +8,7 @@ use std::net::TcpListener;
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
 
     // We need to bring in `reqwest`
     // to perform HTTP requsts against our application.
@@ -16,7 +16,7 @@ async fn health_check_works() {
 
     // Act
     let response = client
-        .get("http://127.0.0.1:3333/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Falied to execute reqwest.");
@@ -27,11 +27,12 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background
-fn spawn_app() {
+fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener)
         .expect("Failed to bind address")
         .into_future();
     let _ = tokio::spawn(server);
-    println!("app spawned.");
+    format!("http://127.0.0.1:{}", port)
 }
