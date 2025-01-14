1eca497c15e508d936740bbef1e6a181281365d5
ohmycloud <ohmycloudy@gmail.com>
2025-01-14 18:42:47 +0800

    add health_check

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,28 @@
 pub mod health_check;
+
+use crate::health_check::health_check;
+use axum::{Router, extract::Path, response::IntoResponse, routing::get};
+use tokio::net::TcpListener;
+
+async fn index() -> impl IntoResponse {
+    "Rust Rocks!"
+}
+
+async fn greet(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello, {}", name)
+}
+
+pub async fn run() -> Result<(), std::io::Error> {
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/", get(index))
+        .route("/{name}", get(greet));
+
+    let listener = TcpListener::bind("0.0.0.0:3333").await.unwrap();
+    println!("Listening on {:?}", listener.local_addr());
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
-use axum::{Router, extract::Path, response::IntoResponse, routing::get};
-use tokio::net::TcpListener;
-use zero2prod_axum::health_check::health_check;
-
-async fn index() -> impl IntoResponse {
-    "Rust Rocks!"
-}
-
-async fn greet(Path(name): Path<String>) -> impl IntoResponse {
-    format!("Hello, {}", name)
-}
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/health_check", get(health_check))
-        .route("/{name}", get(greet))
-        .route("/", get(index));
-    let listener = TcpListener::bind("0.0.0.0:3333").await.unwrap();
-    println!("Listening on {:?}", listener.local_addr());
-
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+async fn main() -> Result<(), std::io::Error> {
+    run().await
 }
