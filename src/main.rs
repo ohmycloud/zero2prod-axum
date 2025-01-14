ec1bf70b57bad113c8ced2c06e2ebfc412fc6ec9
ohmycloud <ohmycloudy@gmail.com>
2025-01-14 16:30:01 +0800

    add root index router

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use axum::{Router, extract::Path, response::IntoResponse, routing::get};
 use tokio::net::TcpListener;
 
+async fn index() -> impl IntoResponse {
+    "Rust Rocks!"
+}
+
 async fn greet(Path(name): Path<String>) -> impl IntoResponse {
     format!("Hello, {}", name)
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/{name}", get(greet));
+    let app = Router::new()
+        .route("/{name}", get(greet))
+        .route("/", get(index));
     let listener = TcpListener::bind("0.0.0.0:3333").await.unwrap();
     println!("Listening on {:?}", listener.local_addr());
 
