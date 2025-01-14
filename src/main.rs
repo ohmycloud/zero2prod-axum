c871e8a79f4c97cc33f2c6346fa79a0086ca86d4
ohmycloud <ohmycloudy@gmail.com>
2025-01-14 16:05:17 +0800

    add Axum web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, extract::Path, response::IntoResponse, routing::get};
+use tokio::net::TcpListener;
+
+async fn greet(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello, {}", name)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/{name}", get(greet));
+    let listener = TcpListener::bind("0.0.0.0:3333").await.unwrap();
+    println!("Listening on {:?}", listener.local_addr());
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
