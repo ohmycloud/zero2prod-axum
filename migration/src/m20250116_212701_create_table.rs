5051d05681543b3de3b26b6bc28e88e552e6a9b3
ohmycloud <ohmycloudy@gmail.com>
2025-01-16 22:06:48 +0800

    add migration for subscriptions

---
+++ migration/src/m20250116_212701_create_table.rs
@@ -0,0 +1,37 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Subscriptions::Table)
+                    .if_not_exists()
+                    .col(pk_uuid(Subscriptions::Id))
+                    .col(text_uniq(Subscriptions::Email))
+                    .col(text(Subscriptions::Name))
+                    .col(timestamp_with_time_zone(Subscriptions::SubscribedAt))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Subscriptions::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+pub(crate) enum Subscriptions {
+    Table,
+    Id,
+    Email,
+    Name,
+    SubscribedAt,
+}
