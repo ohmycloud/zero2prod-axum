2af54cff5c092c64342ecdfe878a6bd9d8849981
ohmycloud <ohmycloudy@gmail.com>
2025-01-17 21:59:17 +0800

    add state to app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use sea_orm::Database;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
@@ -7,5 +8,11 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener)?.await
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = Database::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -4,16 +4,19 @@ use axum::{
     routing::{IntoMakeService, get, post},
     serve::Serve,
 };
+use sea_orm::{DatabaseConnection, sqlx::PgConnection};
 use tokio::net::TcpListener;
 
 pub fn run(
     listener: std::net::TcpListener,
+    connection: DatabaseConnection,
 ) -> Result<Serve<TcpListener, IntoMakeService<Router>, Router>, std::io::Error> {
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
         .route("/", get(index))
-        .route("/{name}", get(greet));
+        .route("/{name}", get(greet))
+        .with_state(connection);
 
     let listener = TcpListener::from_std(listener)?;
     println!("Listening on {:?}", listener.local_addr());
