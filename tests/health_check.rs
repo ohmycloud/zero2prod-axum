624c189a26860d6973429d83b27daad21e3b9295
ohmycloud <ohmycloudy@gmail.com>
2025-01-17 09:01:04 +0800

    test database query with sea-orm

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -77,7 +77,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to fetch data.")
         .expect("No data received.");
-    assert_eq!(saved.status, "confirmed")
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
