75732dddc462f1d5447a244b34d8975946fbd769
ohmycloud <ohmycloudy@gmail.com>
2025-01-18 23:15:04 +0800

    add log

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sea_orm::Database;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -27,15 +27,42 @@ pub async fn subscribe(State(state): State<AppState>, Form(form): Form<FormData>
     let subscriber_id = Uuid::new_v4();
     let subscription = subscriptions::ActiveModel {
         id: Set(subscriber_id),
-        name: Set(form.name),
-        email: Set(form.email),
+        name: Set(form.name.clone()),
+        email: Set(form.email.clone()),
         subscribed_at: Set(DateTimeWithTimeZone::from(Utc::now())),
         status: Set("confirmed".to_string()),
     };
 
-    let _ = Subscriptions::insert(subscription)
+    let request_id = Uuid::new_v4();
+
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.name,
+        form.email
+    );
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
+    let query = Subscriptions::insert(subscription)
         .exec(&state.db_connection)
-        .await
-        .unwrap();
-    StatusCode::OK.into_response()
+        .await;
+    match query {
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            StatusCode::OK.into_response()
+        }
+        Err(e) => {
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
 }
