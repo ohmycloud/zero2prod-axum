77a310f1b65fa355189af64ebd51971e616c1eb5
ohmycloud <ohmycloudy@gmail.com>
2025-01-27 13:05:43 +0800

    support ssl mode

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,5 @@
-use sea_orm::sqlx::postgres::PgConnectOptions;
+use sea_orm::sqlx::ConnectOptions;
+use sea_orm::sqlx::postgres::{PgConnectOptions, PgSslMode};
 use secrecy::{ExposeSecret, SecretString};
 use serde_aux::field_attributes::deserialize_number_from_string;
 
@@ -23,19 +24,29 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(&self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        self.without_db()
+            .database(&self.database_name)
+            .log_statements(tracing_log::log::LevelFilter::Trace)
     }
 }
 
