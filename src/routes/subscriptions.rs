b064562b78a40a2bd852ae8f7aa2b48827a1ece4
ohmycloud <ohmycloudy@gmail.com>
2025-01-30 08:10:12 +0800

    validate the username

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,6 +9,7 @@ use entity::entities::prelude::*;
 use entity::entities::subscriptions;
 use sea_orm::{DatabaseConnection, EntityTrait, Set, prelude::DateTimeWithTimeZone};
 use serde::{Deserialize, Serialize};
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
@@ -60,4 +61,28 @@ pub async fn insert_subscriber(state: &AppState, form: &FormData) -> Result<(), 
             e
         })?;
     Ok(())
+}
+
+/// Returns `true` if the input satisfies all our validation constraints
+/// on subscriber names, `false` otherwise.
+pub fn is_valid_name(s: &str) -> bool {
+    // `.trim()` returns a view over the input `s` without trailing
+    // whitespace-like characters.
+    // `.is_empty` checks if the view contains any character.
+    let is_empty_or_whitespace = s.trim().is_empty();
+    // A grapheme is defined by the Unicode standard as a "user-perceived"
+    // character: `å` is a single grapheme, but it is composed of two characters
+    // (`a` and `̊`).
+    //
+    // `graphemes` returns an iterator over the graphemes in the input `s`.
+    // `true` specifies that we want to use the extended grapheme definition set,
+    // the recommended one.
+    let is_too_long = s.graphemes(true).count() > 256;
+    // Iterate over all characters in the input `s` to check if any of them matches
+    // one of the characters in the forbidden array.
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+    // Return `false` if any of our conditions have been violated
+    !(is_empty_or_whitespace | is_too_long || contains_forbidden_characters)
 }
