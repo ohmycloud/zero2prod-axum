d8561c89d6aa64a6aa7ab2da0a4b20a258ca1c36
ohmycloud <ohmycloudy@gmail.com>
2025-02-11 19:20:07 +0800

    return Ok if the server returns a 200 OK

--- src/email_client.rs
+++ src/email_client.rs
@@ -68,12 +68,14 @@ impl EmailClient {
 
 #[cfg(test)]
 mod tests {
+    use claims::assert_ok;
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::Paragraph;
     use fake::faker::lorem::en::Sentence;
     use fake::{Fake, Faker};
     use secrecy::SecretString;
     use wiremock::Request;
+    use wiremock::matchers::any;
     use wiremock::matchers::header;
     use wiremock::matchers::header_exists;
     use wiremock::matchers::method;
@@ -136,5 +138,35 @@ mod tests {
             .send_email(subscriber_email, &subject, &content, &content)
             .await;
         // Assert
+    }
+
+    #[tokio::test]
+    async fn send_email_succeeds_if_the_server_returns_200() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(
+            mock_server.uri(),
+            sender,
+            SecretString::from(Faker.fake::<String>()),
+        );
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Act
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+        assert_ok!(outcome);
     }
 }
