bf565181e49d2c7d8482641bc68094ed47665fc8
ohmycloud <ohmycloudy@gmail.com>
2025-02-12 11:42:53 +0800

    extracting our startup code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::net::TcpListener;
-
-use sea_orm::{SqlxPostgresConnector, sqlx::postgres::PgPoolOptions};
 use zero2prod::{
     configuration::get_configuration,
-    email_client::EmailClient,
-    startup::run,
+    startup::build,
     telemetry::{get_subscriber, init_subscriber},
 };
 
@@ -15,28 +11,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    let _ = listener.set_nonblocking(true);
-
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let db_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let db_connection = SqlxPostgresConnector::from_sqlx_postgres_pool(db_pool);
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    run(listener, db_connection, email_client)?.await
+    let server = build(configuration).await?;
+    server.await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,5 @@
 use crate::{
+    configuration::{Settings, get_configuration},
     email_client::EmailClient,
     routes::{AppState, health_check::*, subscribe},
 };
@@ -8,7 +9,7 @@ use axum::{
     serve::Serve,
 };
 use axum_tracing_opentelemetry::middleware::OtelAxumLayer;
-use sea_orm::DatabaseConnection;
+use sea_orm::{DatabaseConnection, SqlxPostgresConnector, sqlx::postgres::PgPoolOptions};
 use tokio::net::TcpListener;
 
 pub fn run(
@@ -37,4 +38,33 @@ pub fn run(
 
     let server = axum::serve(listener, app.into_make_service());
     Ok(server)
+}
+
+pub async fn build(
+    configuration: Settings,
+) -> Result<Serve<TcpListener, IntoMakeService<Router>, Router>, std::io::Error> {
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = std::net::TcpListener::bind(address).expect("Failed to bind random port");
+    let _ = listener.set_nonblocking(true);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let db_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+    let db_connection = SqlxPostgresConnector::from_sqlx_postgres_pool(db_pool);
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    run(listener, db_connection, email_client)
 }
