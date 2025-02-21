eaf3a87eb16812982dd19c5568a42ad32d83b765
ohmycloud <ohmycloudy@gmail.com>
2025-02-21 17:25:10 +0800

    test a fatal database error

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,5 +1,6 @@
 use entity::entities::prelude::*;
 use migration::{Migrator, MigratorTrait};
+use sea_orm::ConnectionTrait;
 use sea_orm::{EntityTrait, SqlxPostgresConnector, sqlx::postgres::PgPoolOptions};
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
@@ -153,4 +154,22 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     // The two links should be identical
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
+}
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le％20guin&email=ursula_le_guin%40gmail.com";
+    // Sabotage the database
+    app.db_connection
+        .execute_unprepared("ALTER TABLE subscription_tokens DROP COLUMN subscription_token;")
+        .await
+        .unwrap();
+
+    // Act
+    let response = app.post_subscriptions(body.into()).await;
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 500);
 }
