f5c6ffacb8355f21205868a7e4610497d71de5b1
ohmycloud <ohmycloudy@gmail.com>
2025-02-23 00:35:08 +0800

    select email from subscribers

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,9 @@
 use axum::response::{IntoResponse, Response};
+use entity::entities::subscriptions::Entity;
+use entity::entities::{prelude::*, subscriptions};
 use reqwest::StatusCode;
+use sea_orm::prelude::*;
+use sea_orm::{DatabaseConnection, EntityTrait, QueryFilter};
 
 #[derive(Debug, serde::Deserialize)]
 pub struct BodyData {
@@ -13,7 +17,27 @@ pub struct Content {
     text: String,
 }
 
+struct ConfirmedSubscriber {
+    email: String,
+}
+
 // Dummy implementation
 pub async fn publish_newsletter(_body: axum::Json<BodyData>) -> Response {
     StatusCode::OK.into_response()
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(db_connection))]
+async fn get_confirmed_subscribers(
+    db_connection: &DatabaseConnection,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let subscribers = Subscriptions::find()
+        .filter(subscriptions::Column::Status.eq("confirmed"))
+        .all(db_connection)
+        .await
+        .expect("Failed to fetch data.")
+        .into_iter()
+        .map(|model| ConfirmedSubscriber { email: model.email })
+        .collect::<Vec<ConfirmedSubscriber>>();
+
+    Ok(subscribers)
 }
