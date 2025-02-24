925f7fab56cfda3a9992b4e6e49b00e758f9c586
ohmycloud <ohmycloudy@gmail.com>
2025-02-24 13:50:12 +0800

    add LoginError

--- src/routes/admin/login.rs
+++ src/routes/admin/login.rs
@@ -1,4 +1,8 @@
+use crate::authentication::{AuthError, Credentials, validate_credentials};
+use crate::routes::{AppState, error_chain_fmt};
 use axum::Form;
+use axum::extract::State;
+use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse, Redirect, Response};
 use secrecy::SecretString;
 
@@ -8,8 +12,53 @@ pub struct FormData {
     password: SecretString,
 }
 
-pub async fn login(Form(form): Form<FormData>) -> Response {
-    Redirect::to("/").into_response()
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Unexpected error")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl IntoResponse for LoginError {
+    fn into_response(self) -> Response {
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED.into_response(),
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        }
+    }
+}
+
+#[tracing::instrument(
+    skip(form, state),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(
+    State(state): State<AppState>,
+    Form(form): Form<FormData>,
+) -> Result<Response, LoginError> {
+    let credentials = Credentials {
+        username: form.username,
+        password: form.password,
+    };
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+    let user_id = validate_credentials(credentials, &state.db_connection)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
+    Ok(Redirect::to("/").into_response())
 }
 
 pub async fn login_form() -> Response {
