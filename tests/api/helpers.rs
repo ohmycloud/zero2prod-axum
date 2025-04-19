f312bffb6f8127c9926749104918eb78cc6e0205
ohmycloud <ohmycloudy@gmail.com>
2025-04-19 15:34:36 +0800

    Finish 10.7.5.4: Reject Unauthenticated Users

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -48,12 +48,16 @@ pub struct ConfirmationLinks {
 }
 
 impl TestApp {
-    pub async fn get_admin_dashboard_html(&self) -> String {
+    pub async fn get_admin_dashboard(&self) -> reqwest::Response {
         self.api_client
             .get(&format!("{}/admin/dashboard", &self.address))
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+    pub async fn get_admin_dashboard_html(&self) -> String {
+        self.get_admin_dashboard()
+            .await
             .text()
             .await
             .expect("Failed to read response body.")
