94607b2c3ad40c1890c7abef1b71f6a9db3c996e
ohmycloud <ohmycloudy@gmail.com>
2025-04-22 08:26:52 +0800

    retrieve saved responses

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -1,13 +1,14 @@
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
-use crate::idempotency::IdempotencyKey;
+use crate::idempotency::{IdempotencyKey, get_saved_response};
 use crate::routes::{AppState, error_chain_fmt};
-use crate::utils::e400;
+use crate::utils::{e400, e500};
 use anyhow::Context;
 use axum::extract::State;
 use axum::http::HeaderValue;
 use axum::response::{IntoResponse, Response};
 use axum::{Extension, Form};
+use axum_messages::Messages;
 use entity::entities::{prelude::*, subscriptions};
 use reqwest::StatusCode;
 use sea_orm::prelude::*;
@@ -64,10 +65,21 @@ impl IntoResponse for PublishError {
 )]
 pub async fn publish_newsletter(
     State(state): State<AppState>,
+    flash: Messages,
     user_id: Extension<UserId>,
     Form(form): Form<FormData>,
 ) -> Result<Response, PublishError> {
     let idempotency_key: IdempotencyKey = form.idempotency_key.try_into().map_err(e400).unwrap();
+    // Return early if er have a saved response in the database
+    if let Some(saved_response) =
+        get_saved_response(&state.db_connection, &idempotency_key, *user_id.0)
+            .await
+            .map_err(e500)
+            .unwrap()
+    {
+        flash.info("The newsletter issue has been published!");
+        return Ok(saved_response);
+    }
 
     tracing::info!("Publishing a newsletter issue: {}", *user_id);
     let subscribers = get_confirmed_subscribers(&state.db_connection).await?;
